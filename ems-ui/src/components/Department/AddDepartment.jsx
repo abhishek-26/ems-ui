17c785cd46d1b25d5593ebbc74cec103baa1c2fc
Abhishek Singh <abhishek.singh3@jsw.in>
2024-11-04 17:51:54 +0530

    Add Department Page Css done

--- ems-ui/src/components/Department/AddDepartment.jsx
+++ ems-ui/src/components/Department/AddDepartment.jsx
@@ -1,13 +1,30 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export const AddDepartment = () => {
-  const [email, setEmail] = useState("");  
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
   const [error, setError] = useState(null);
 
+  const [department, setDepartment] = useState({
+    dep_name: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(`name is ${name} and description is ${value}`);
+    setDepartment({ ...department, [name]: value });
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log(
+      `department name is ${e.target.dep_name} and description is ${e.target.description}`
+    );
+    alert(
+      `department name is ${e.target.dep_name} and description is ${e.target.description}`
+    );
     try {
       const response = await axios.post("http://localhost:8080", {
         email,
@@ -20,46 +37,44 @@ export const AddDepartment = () => {
   };
 
   return (
-    <div
-      className="flex flex-col items-center justify-center 
-           h-screen space-y-6"
-    >
-      <h2 className="text-3xl font-bold mb-4">
-        Add new Department
-      </h2>
-      <div className="bg-white p-6 w-80 border border-gray-300">
-        <h2 className="text-2xl font-bold mb-4">Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-gray-700" htmlFor="email">
-              Department Name
-            </label>
-            <input
-              className="w-full px-3 py-2 border"
-              id="departmentName"
-              type="text"
-              placeholder="IT"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-          <label>
-      Description
-      <textarea name="description" placeholder="Description.."/>
-    </label>
-          </div>
-          <div className="mb-4">
-            <button
-              className="w-full bg-teal-600 text-white py-2"
-              type="submit"
-            >
-              Add Department
-            </button>
-          </div>
-          {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
-        </form>
-      </div>
+    <div className="max-w-3xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md w-96">
+      <h2 className="text-3xl font-bold mb-4">Add new Department</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700" htmlFor="dep_name">
+            Department Name
+          </label>
+          <input
+            className="w-full px-3 py-2 border"
+            id="dep_name"
+            name="dep_name"
+            type="text"
+            placeholder="IT"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mt-3">
+          <label
+            htmlFor="description"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Description
+          </label>
+          <textarea
+            name="description"
+            className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+            rows="4"
+            placeholder="Description.."
+            onChange={handleChange}
+          />
+        </div>
+        <button
+          className="w-full mt-6 bg-teal-600 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded-md"
+          type="submit"
+        >
+          Add Department
+        </button>
+      </form>
     </div>
   );
 };
